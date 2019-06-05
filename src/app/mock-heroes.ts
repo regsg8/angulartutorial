fe043580c9db728a1438792691b0bd49084d3449
“Reg <regsg8@gmail.com>
2019-06-05 15:25:06 -0600

    Added Hero List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Iron Man' },
+    { id: 12, name: 'Thor' },
+    { id: 13, name: 'Vision' },
+    { id: 14, name: 'Black Widow' },
+    { id: 15, name: 'Bruce Banner' },
+    { id: 16, name: 'Spiderman' },
+    { id: 17, name: 'Captain America' },
+    { id: 18, name: 'Hawkeye' },
+    { id: 19, name: 'Scarlet Witch' },
+    { id: 20, name: 'War Machine' },
+] 
